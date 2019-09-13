221c3a0de4b354f21d791ce9727161df5712b43b
vbachev <vasil@vasilbachev.com>
2019-09-14 00:58:18 +0300

    Minimal giphy example

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
 import React from 'react'
 
-const App = () => (
-  <div>Hello world</div>
-)
+const url = 'https://api.giphy.com/v1/gifs/search?api_key=CdRKiCMbTnt9CkZTZ0lGukSczk6iT4Z6&limit=5&offset=0&rating=G&lang=en&q='
+
+const App = () => {
+  const [images, setImages] = React.useState([])
+
+  const searchByKeyword = async event => {
+    event.preventDefault()
+    const keyword = document.getElementById('keyword').value
+    const response = await (await fetch(url + keyword)).json()
+    setImages(response.data.map(item => item.images.preview_gif.url))
+  }
+
+  return (
+    <>
+      <form onSubmit={searchByKeyword}>
+        <input id="keyword" />
+      </form>
+      <ul>
+        {images.map(image => (
+          <li key={image}>
+            <img src={image} />
+          </li>
+        ))}
+      </ul>
+    </>
+  )
+}
 
 export default App
