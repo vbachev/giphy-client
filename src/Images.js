06df6edf4ea9a8989c57bd7dc4550505ebc3b280
vbachev <vasil@vasilbachev.com>
2019-09-15 02:25:27 +0300

    Add layout buttons

--- src/Images.js
+++ src/Images.js
@@ -3,15 +3,33 @@ import Image from './Image'
 
 // @TODO:
 // - listen to scroll events and trigger loading additional images
-// - display layout buttons (grid/list)
+// - move layout state up to App
+// - fix mobile layout
 
 const Images = ({ images, keyword, total }) => {
+  const [layout, setLayout] = React.useState('list')
   return (
-    <div className='images-container'>
-      <div className='images-summary'>
-        <span className='images-keyword'>{keyword}</span>
-        <span className='images-total'>{total} GIFs</span>
+    <div className={`images-container layout-${layout}`}>
+
+      <div className='images-layout'>
+        Display as:
+        <button className='layout-button layout-grid' onClick={() => setLayout('grid')}>
+          Grid
+        </button>
+        <button className='layout-button layout-list' onClick={() => setLayout('list')}>
+          List
+        </button>
       </div>
+
+      <div className='images-summary'>
+        <span className='images-keyword'>
+          {keyword}
+        </span>
+        <span className='images-total'>
+          {total} GIFs
+        </span>
+      </div>
+
       <ul className='images-list'>
         {images.map(image => (
           <li key={image.title}>
