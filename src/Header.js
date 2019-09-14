84a94c59e4e4f30a4186ac9dc53865b7024727ff
vbachev <vasil@vasilbachev.com>
2019-09-14 22:18:01 +0300

    Add logo

--- src/Header.js
+++ src/Header.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <h1>GIPHY</h1>
+    <header class="header">
+      <div className="logo-container">
+        <img className="logo-mark" src="giphy-logo.png" alt="Giphy Client"/>
+        <h1 className="logo-type">
+          GIPHY
+        </h1>
+      </div>
+    </header>
   )
 }
 
