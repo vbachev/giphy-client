3c3a901302a02181653da8bdd1b8edec3d308761
vbachev <vasil@vasilbachev.com>
2019-09-15 01:09:29 +0300

    Add loading animation

--- src/App.js
+++ src/App.js
@@ -2,23 +2,24 @@ import React from 'react'
 import Logo from './Logo'
 import Images from './Images'
 import SearchBar from './SearchBar'
+import Loader from './Loader'
 
 const url = 'https://api.giphy.com/v1/gifs/search?api_key=CdRKiCMbTnt9CkZTZ0lGukSczk6iT4Z6&limit=5&offset=0&rating=G&lang=en&q='
 
 // @TODO:
 // - extract API calls to a separate module
 // - load some initial images
-// - animation while waiting for image requests
 // - keep local state for pagination
-// - message for no results
 
 const App = () => {
   const [images, setImages] = React.useState([])
   const [keyword, setKeyword] = React.useState('')
   const [total, setTotal] = React.useState(0)
+  const [loading, setLoading] = React.useState(false)
 
   const searchByKeyword = async keyword => {
     setKeyword(keyword)
+    setLoading(true)
     const response = await (await fetch(url + keyword)).json()
     setTotal(response.pagination.total_count)
     setImages(response.data.map(item => ({
@@ -27,13 +28,27 @@ const App = () => {
       width: item.images.fixed_width.width,
       height: item.images.fixed_width.height
     })))
+    setLoading(false)
   }
 
   return (
     <div className='container'>
       <Logo />
       <SearchBar onSearch={searchByKeyword} />
-      <Images images={images} total={total} keyword={keyword} />
+      
+      {loading && (
+        <Loader />
+      )}
+      
+      {!loading && !!total && (
+        <Images images={images} total={total} keyword={keyword} />
+      )}
+
+      {!loading && !total && (
+        <div className='no-results'>
+          No GIFs to show
+        </div>
+      )}
     </div>
   )
 }
---
+++ src/Loader.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Loader = () => (
+  <div className='loader'>
+    <div className='loader-square'></div>
+    <div className='loader-square'></div>
+    <div className='loader-square'></div>
+    <div className='loader-square'></div>
+    <div className='loader-square'></div>
+  </div>
+)
+
+export default Loader
