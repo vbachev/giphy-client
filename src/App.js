8d8b9ed9e653d2e02fb5e88dd3ff9c2aef7749f4
vbachev <vasil@vasilbachev.com>
2019-09-15 11:14:13 +0300

    Add infinite scroll

--- src/App.js
+++ src/App.js
@@ -7,40 +7,73 @@ import Loader from './Loader'
 
 // @TODO:
 // - load some initial images
-// - keep local state for pagination
+
+const debounce = (delay, fn) => {
+  let timeoutReference
+  return (...args) => {
+    clearTimeout(timeoutReference)
+    timeoutReference = setTimeout(() => fn(...args), delay)
+  }
+}
 
 const App = () => {
-  const [images, setImages] = React.useState([])
-  const [keyword, setKeyword] = React.useState('')
-  const [total, setTotal] = React.useState(0)
-  const [loading, setLoading] = React.useState(false)
+  const [state, setState] = React.useState({
+    images: [],
+    keyword: '',
+    total: 0,
+    loading: false
+  })
 
   const searchByKeyword = async keyword => {
-    setKeyword(keyword)
-    setLoading(true)
+    setState({ ...state, loading: true })
     const response = await giphy.fetchImages({ term: keyword })
-    setTotal(response.total)
-    setImages(response.images)
-    setLoading(false)
+    setState({
+      ...state,
+      keyword,
+      total: response.total,
+      images: response.images,
+      loading: false
+    })
   }
+
+  const loadMoreImages = async () => {
+    setState({ ...state, loading: true })
+    const response = await giphy.fetchImages({ term: state.keyword, offset: state.images.length })
+    setState({
+      ...state,
+      total: response.total,
+      images: state.images.concat(response.images),
+      loading: false
+    })
+  }
+
+  const onScroll = debounce(100, () => {
+    const { clientHeight, offsetHeight } = document.documentElement
+    const isAtBottom = clientHeight + window.scrollY === offsetHeight
+    const hasMoreImagesToLoad = state.images.length < state.total
+    if (isAtBottom && hasMoreImagesToLoad) loadMoreImages()
+  })
+
+  React.useEffect(() => {
+    window.addEventListener('scroll', onScroll)
+    return () => window.removeEventListener('scroll', onScroll)
+  }, [state]) // @TODO: optimise this as it rebinds listeners whenever state changes :(
 
   return (
     <div className='container'>
       <Logo />
       <SearchBar onSearch={searchByKeyword} />
       
-      {loading && (
-        <Loader />
-      )}
-      
-      {!loading && !!total && (
-        <Images images={images} total={total} keyword={keyword} />
-      )}
-
-      {!loading && !total && (
+      {state.total ? (
+        <Images images={state.images} total={state.total} keyword={state.keyword} />
+      ) : (
         <div className='no-results'>
           No GIFs to show
         </div>
+      )}
+      
+      {state.loading && (
+        <Loader />
       )}
     </div>
   )
